0fe96d57dc2f299dc925d5b1ea5bbf58e51c6a11
bogachenko <bogachenkove@gmail.com>
2019-02-08 02:03:30 +1100

    Update building.sh

--- tools/building.sh
+++ tools/building.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 git pull
 perl ./addChecksum.pl ../presstheattack.txt
+perl ./updateDateString.pl ../presstheattack.txt
 git status
 git commit -a -m "Update presstheattack.txt"
 git push
